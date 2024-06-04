13aa47f29061321039ad11b8dfe26c09eb3db060
Parminder Singh <parminder.singh@thewitslab.com>
2024-06-04 14:26:00 +0530

    [skip ci]

--- start.sh
+++ start.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-
+Date=$(date | sed 's/ //g')
+docker-compose down
 # Check if the Vault container is already running
 if docker ps -a --filter "name=vault" | grep -q 'Up'; then
     echo "Vault container is already running. Skipping start."
 else
     echo "Starting Vault container..."
-    docker-compose up vault -d
+    sudo cp -r ~/ALL_VOLUMES ~/ALL_VOLUMES_$Date
+    sudo cp ~/Sunbird-Rc/keys.txt ~/keys_$Date.txt
+    sudo rm -rf ~/ALL_VOLUMES/*
+    mkdir ~/ALL_VOLUMES/vault-data
+    docker run --interactive -v /home/ubuntu/ALL_VOLUMES/vault-data:/vault --rm --tty ubuntu:latest chown -R 10001 /vault
+    docker-compose up -d --build vault 
     bash setup_vault.sh docker-compose.yml vault
 fi
 
@@ -24,28 +30,27 @@ VAULT_TOKEN=$(awk '/Initial Root Token:/ {print $4}' keys.txt)
 
 # Print the Vault token
 echo "Vault Token: $VAULT_TOKEN"
-
     
 # Unseal the Vault
 echo "Unsealing Vault..."
 docker-compose exec -T vault vault operator unseal $KEY1
 docker-compose exec -T vault vault operator unseal $KEY2
 docker-compose exec -T vault vault operator unseal $KEY3
-
 # Enable the KV secrets engine
 echo "Enabling KV secrets engine..."
+docker-compose exec -e VAULT_TOKEN=$VAULT_TOKEN -T vault vault secrets disable kv
 docker-compose exec -e VAULT_TOKEN=$VAULT_TOKEN -T vault vault secrets enable -path=kv kv-v2
 # echo "VAULT_TOKEN=hvs.ubAeQf0e5GMFJNjauy1KYaCa" >> .en
 echo "Vault Token: $VAULT_TOKEN"
 
 echo "Updating .env file with new VAULT_TOKEN value..."
-sed -i '' "s/^VAULT_TOKEN=.*/VAULT_TOKEN=$VAULT_TOKEN/" .env
+sed -i "s/^VAULT_TOKEN=.*/VAULT_TOKEN=$(printf '%s\n' "$VAULT_TOKEN" | sed 's/[\/&]/\\&/g')/" .env
+#sed -i '' "s/^VAULT_TOKEN=.*/VAULT_TOKEN=$VAULT_TOKEN/" .env
 cat .env
-docker-compose down identity
-docker-compose up -d identity
-
+docker-compose up -d --build identity
+docker-compose exec -e VAULT_TOKEN=$VAULT_TOKEN -T identity echo "exporting vault token in identity"
 # Start the identity and credential schema services
 echo "Starting identity and credential schema services..."
-docker-compose up credential schema nginx -d
-
+docker-compose up -d --build credential schema
+docker-compose up -d --build nginx
 echo "Restart process completed."
